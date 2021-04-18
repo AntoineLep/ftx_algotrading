dde337898048664a312b29ee069d38c345e71f72
AntoineLep <6230724+AntoineLep@users.noreply.github.com>
2021-04-18 12:28:37 +0200

    update gitignore to allow template files into private directories

---
+++ config/private/ftx_config_template.py
@@ -0,0 +1,15 @@
+"""
+Go to https://ftx.com/profile
+Select Api tab and create an API key for the sub account of your choice
+Fill the api dictionary fields bellow
+Rename This file in ftx_config.py
+"""
+
+ws_endpoint = "wss://ftx.com/ws/"
+rest_endpoint = "https://ftx.com/api/"
+
+api = {
+    "key": "YOUR API KEY HERE",
+    "secret": "YOUR API SECRET HERE",
+    "sub_account": "YOUR SUB ACCOUNT NAME HERE"
+}
---
+++ strategies/twitter_elon_musk_doge_tracker/config/private/twitter_config_template.py
@@ -0,0 +1,12 @@
+"""
+Go to https://developer.twitter.com/en/portal/dashboard
+Create a new project and app (or link a new app to an existing project) and get an API key
+Fill the api dictionary fields bellow
+Rename This file in twitter_config.py
+"""
+
+api = {
+    "key": "YOUR API KEY HERE",
+    "secret_key": "YOUR API SECRET KEY HERE",
+    "bearer_token": "YOUR BEARER TOKEN HERE"
+}
